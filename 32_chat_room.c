6f58e7d84f1bbdc6308aa0ec474552af98b80943
rh_abir <rhabir2006@gmail.com>
2026-09-23 09:47:38 +0600

    codeforces solution

---
+++ 32_chat_room.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char arr[100];
+    scanf("%s",arr);
+
+    char brr[5] = "hello";
+
+    int i=0;
+    int j=0;
+
+    while(arr[i]=='\0' && j<5)
+    {
+        if(arr[i] == brr[j])
+          {
+            j++;
+          }
+    i++;
+    }
+
+    if(j == 5){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+
+    return 0;
+}
