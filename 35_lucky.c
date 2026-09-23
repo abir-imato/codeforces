a4dbb8515e1d6a7a7af58fbea7aef125c42a6f7e
rh_abir <rhabir2006@gmail.com>
2026-09-23 12:09:09 +0600

    solved lucky division problem

--- 35_lucky.c
+++ 35_lucky.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 
+int isLucky(int x)
+{
+    while (x > 0)
+    {
+        int digit = x % 10;
+        if (digit != 4 && digit != 7)
+        {
+            return 0;
+        }
+        x = x / 10;
+    }
+    return 1;
+}
+
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(int i=1;n>0;i++)
-       {
-         
-       }
+    int found = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (isLucky(i) && n % i == 0)
+        {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found)
+        printf("YES");
+    else
+        printf("NO");
+
     return 0;
 }
