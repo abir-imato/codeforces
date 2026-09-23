384a27aad2f03980eb3333750936fdf9d5359c1a
rh_abir <rhabir2006@gmail.com>
2026-09-24 00:26:27 +0600

    codeforces solution

--- 42_pangram.c
+++ 42_pangram.c
@@ -6,7 +6,10 @@ int main()
 {
     char arr[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-    char brr[100];
+    int n;
+    scanf("%d",&n);
+
+    char brr[n];
     scanf("%s",brr);
 
     int a = strlen(brr);
@@ -15,7 +18,7 @@ int main()
 
     for(int i = 0; i < 26; i++)
 {
-    for(int j = 0; j < a; j++)
+    for(int j = 0; j < n; j++)
     {
         brr[j] = tolower(brr[j]);
         if(brr[j] == arr[i])
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main()
 {
+    char arr[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
     int n;
     scanf("%d",&n);
-    int m;
-    int magnet = 0;
+
+    char brr[n];
+    scanf("%s",brr);
+
+    int a = strlen(brr);
+
     int count=0;
 
-    for(int i=1;i <= n ;i++)
-       {
-         scanf("%d",&m);
-         if(magnet != m)
-           {
+    for(int i = 0; i < 26; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        brr[j] = tolower(brr[j]);
+        if(brr[j] == arr[i])
+        {
             count++;
-            magnet=m;
-           }
-       }
+            break;
+        }
+    }
+}
+      if(count == 26)
+        {
+            printf("YES");
+        }
 
-       printf("%d",count);
+        else{
+            printf("NO");
+        }
     return 0;
 }
