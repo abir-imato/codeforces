1a59f2725da7a0fab8fee7ca98bc34a472eeca12
rh_abir <rhabir2006@gmail.com>
2026-09-24 00:44:36 +0600

    codeforces solution

---
+++ 43_gravity_cube.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(int i=1;i<=n;i++)
+       {
+         scanf("%d",&arr[i]);
+       }
+
+    for(int i=1;i<=n;i++)
+       {
+        for(int j=i+1;j<=n;j++)
+           {
+            if(arr[i] >= arr[j])
+              {
+                int s=arr[j];
+                arr[j]=arr[i];
+                arr[i]=s;
+              }
+           }
+       }  
+       
+       for(int i=1;i<=n;i++)
+          {
+            printf("%d ",arr[i]);
+          }
+    return 0;
+}
