2c2b5a529ecbba454be2444693659ba63207bd06
rh_abir <rhabir2006@gmail.com>
2026-09-26 23:49:08 +0600

    codeforces solution

--- 48_dragon.c
+++ 48_dragon.c
@@ -2,18 +2,38 @@
 
 int main()
 {
-    int d;
-    scanf("%d",&d);
+    int k,l,m,n,d;
+
+    scanf("%d",&k);
+     scanf("%d",&l);
+      scanf("%d",&m);
+       scanf("%d",&n);
+        scanf("%d",&d);
 
     int arr[d];
+
+    int sum=0;
 
     for(int i=0;i<d;i++)
     {
        arr[i]=0; 
     }
 
-    printf("%d",arr[1]);
+    for(int i=0;i<d;i++)
+       {
+        int d=i+1;
+         if(d%k==0 || d%l==0 || d%m==0 || d%n==0)
+           {
+             arr[i]=1;
+           }
+       }
 
+    for(int i=0;i<d;i++)
+       {
+         sum = sum + arr[i];
+       }   
+
+       printf("%d",sum);
 
     return 0;
 }
